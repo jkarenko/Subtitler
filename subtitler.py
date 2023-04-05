ff68528519b241e124ed460307c327e9a9676701
Karenko, Juho <juho.karenko@f-secure.com>
2023-04-05 19:14:59 +0300

    add denoiser and mix features

--- subtitler.py
+++ subtitler.py
@@ -1,18 +1,21 @@
 import math
 
+import numpy as np
 from yt_dlp import YoutubeDL
 import pywhisper
 from pywhisper.utils import write_srt, write_vtt, write_txt
 import datetime
 from pathlib import Path
 import argparse
-# from spleeter.audio.adapter import AudioAdapter
-# from spleeter.separator import Separator
+from spleeter.audio.adapter import AudioAdapter
+from spleeter.separator import Separator
 import keyring
 # import os
 import openai
 from summarizer import Summarizer, TransformerSummarizer
 import time
+import noisereduce as nr
+from scipy.io import wavfile
 
 # import matchering as mg
 # from pydub import AudioSegment
@@ -31,6 +34,8 @@ def parse_args():
     source_file.add_argument("--url", help="URL of the video to be transcribed")
     source_file.add_argument("--filename", help="Path to the video file to be transcribed")
     source_file.add_argument("--text_file", help="Text to be summarised")
+    source_file.add_argument("--denoise", nargs="+", help="Denoise audio files")
+    parser.add_argument("--mix", type=str, help="Output file name of the mixed audio")
     parser.add_argument("--model", type=str, default="base", help="Model to use")
     parser.add_argument("--lang", type=str, help="Language of the video")
     parser.add_argument("--audio_only", action="store_true", help="Video format to download")
@@ -83,10 +88,14 @@ def summarise_text(text,
         summaries = [summarise_text(part) for part in text_parts]
     if summaries:
         text = "\n".join(summaries)
-    response = openai.Completion.create(
-        model="text-davinci-003",
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
         # prompt=f"{text}\nTo re-iterate, these are the steps:{stop}",
-        prompt=f"{text}\nIn summary:{stop}",
+        # prompt=f"{text}\nIn summary:{stop}",
+        messages=[
+            {"role": "system", "content": "You are a text summarizer. Summarize the following text:"},
+            {"role": "user", "content": text},
+        ],
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=top_p,
@@ -94,7 +103,7 @@ def summarise_text(text,
         presence_penalty=presence_penalty,
         stop=stop
     )
-    return response.choices[0].text
+    return response.choices[0].message.content
 
 
 def download_video(url, video_format):
@@ -106,7 +115,7 @@ def download_video(url, video_format):
 
 
 def spleeter_separate(filename):
-    separator = Separator('spleeter:5stems')
+    separator = Separator('spleeter:2stems')
     audio_loader = AudioAdapter.default()
     sr = 48000
     waveform, _ = audio_loader.load(filename, sample_rate=sr)
@@ -117,16 +126,22 @@ def spleeter_separate(filename):
     print("Audio separated")
 
 
-def speech_to_text(url=None, filename=None, lang=None, model="base", video_format="best", audio_only=False,
+def speech_to_text(url=None, filename=None, denoise=None, mix=None, lang=None, model="base", video_format="best",
+                   audio_only=False,
                    output_format="srt", device="cuda", translate=False, subtitles=False, prompt="", split=False,
                    summary=False, text_file=None, **kwargs):
+    if denoise is not None:
+        denoised = denoise_audio(denoise)
+        if mix:
+            mix_audio(denoised, mix)
+
     if text_file is not None:
         with open(text_file, "r", encoding="utf-8") as f:
             text = f.read()
         summary = summarise_text(text)
         print(summary)
         return
-    if url is None and filename is None:
+    if not any([url, filename]):
         raise ValueError("Either url or filename must be specified")
 
     if url is not None:
@@ -192,6 +207,53 @@ def create_srt_data(result):
         text = " ".join(text).replace("\n ", "\n")
         srt += f"{seq}\n{start} --> {end}\n{text}\n\n"
     return srt
+
+
+def denoise_audio(filenames):
+    denoised = []
+    for filename in filenames:
+        print("Denoising", filename, "...")
+        sr, audio = wavfile.read(filename)
+        # reduced_noise = nr.reduce_noise(y=audio, sr=sr)
+        reduced_noise = audio
+        wavfile.write(
+            new_name := f"{Path(filename).stem}_denoised{Path(filename).suffix}",
+            sr,
+            reduced_noise,
+        )
+        denoised.append(new_name)
+    return denoised
+
+
+def mix_audio(filenames, output_filename):
+    # List of input WAV files
+    input_files = filenames
+    print("Mixing", ", ".join(input_files), "to", output_filename, "...")
+
+    # Read the first file to get the sampling rate and initialize the mixed data
+    rate, mixed_data = wavfile.read(input_files[0])
+    mixed_data = mixed_data.astype(np.int32)
+
+    # Read and mix the remaining files
+    for file in input_files[1:]:
+        _, data = wavfile.read(file)
+        mixed_data += data.astype(np.int32)
+
+    # Average the mixed data
+    mixed_data = mixed_data // len(input_files)
+
+    # Ensure that the mixed samples are within the appropriate range
+    if mixed_data.dtype == np.int16:
+        mixed_data = np.clip(mixed_data, -32768, 32767)
+    elif mixed_data.dtype == np.int32:
+        mixed_data = np.clip(mixed_data, -2147483648, 2147483647)
+    # Add more cases if needed (e.g., np.int8, np.int64)
+
+    # Convert the mixed samples back to the original data type
+    mixed_data = mixed_data.astype(np.int16)
+
+    # Write the mixed samples to a new WAV file
+    wavfile.write(output_filename, rate, mixed_data)
 
 
 def main(**kwargs):
